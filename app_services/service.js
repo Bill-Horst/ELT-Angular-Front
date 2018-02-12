e846c5bc36a6c9efa4dd3a34ce3bb65431062b4e
Bill Horst <bill@billhorst.com>
2018-02-11 21:07:29 -0700

    added config file and improved ui with buttons

--- app_services/service.js
+++ app_services/service.js
@@ -9,9 +9,13 @@
 
   function Service($http, $q) {
 
+    let baseUrl = '';
     let service = {};
-    // let baseUrl = 'https://frozen-sierra-32417.herokuapp.com/';
-    let baseUrl = 'http://localhost:3000/';
+    if (deployedOnAWS) {
+      baseUrl = 'https://frozen-sierra-32417.herokuapp.com/';
+    } else {
+      baseUrl = 'http://localhost:3000/';
+    }
 
     service.get = get;
     service.post = post;
@@ -21,33 +25,33 @@
     return service;
 
     function get(path) {
-        return $http({
-          method: 'GET',
-          url: baseUrl + path
-        }).then(handleSuccess, handleError);
+      return $http({
+        method: 'GET',
+        url: baseUrl + path
+      }).then(handleSuccess, handleError);
     }
 
     function post(path, object) {
-        return $http({
-          method: 'POST',
-          url: baseUrl + path,
-          data: object
-        }).then(handleSuccess, handleError);
+      return $http({
+        method: 'POST',
+        url: baseUrl + path,
+        data: object
+      }).then(handleSuccess, handleError);
     }
 
     function update(path, object) {
-        return $http({
-          method: 'PUT',
-          url: baseUrl + path,
-          data: object
-        }).then(handleSuccess, handleError);
+      return $http({
+        method: 'PUT',
+        url: baseUrl + path,
+        data: object
+      }).then(handleSuccess, handleError);
     }
 
     function destroy(path) {
-        return $http({
-          method: 'DELETE',
-          url: baseUrl + path
-        }).then(handleSuccess, handleError);
+      return $http({
+        method: 'DELETE',
+        url: baseUrl + path
+      }).then(handleSuccess, handleError);
     }
 
     function handleSuccess(response) {
---
+++ config.js
@@ -0,0 +1,2 @@
+// if deployed on AWS, set to true, otherwise, set to false
+deployedOnAWS = true;
